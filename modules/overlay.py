fbdb287105380fb204644d231c971555582443a2
Ruslan865 <babashov666@gmail.com>
2026-04-06 08:44:22 -0700

    Create overlay.py

---
+++ modules/overlay.py
@@ -0,0 +1,8 @@
+# overlay.py
+class Overlay:
+    def __init__(self):
+        print("Overlay system initialized")
+    
+    def update(self, frame):
+        # GIF overlay logic skeleton
+        pass
