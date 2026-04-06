e09435546510a06c20eb27e714c62b0f655021a7
Ruslan865 <babashov666@gmail.com>
2026-04-06 08:42:04 -0700

    Add Camera class with basic methods

---
+++ modules/camera.py
@@ -0,0 +1,11 @@
+# camera.py
+class Camera:
+    def __init__(self):
+        print("Camera initialized")
+    
+    def start_preview(self):
+        print("Camera preview started")
+    
+    def get_frame(self):
+        # Skeleton frame
+        return None
